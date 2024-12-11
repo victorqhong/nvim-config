ed13a0ace5c6a3b0d556fe80d1bf81210701191c
Victor Hong <victor@victorqhong.com>
2024-12-10 23:21:49 -0500

    Add workstation changes

--- init.lua
+++ init.lua
@@ -4,7 +4,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -18,7 +19,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
